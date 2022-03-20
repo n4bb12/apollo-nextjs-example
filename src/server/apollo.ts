8bcf05958b624686f29041aed003d66fa968b93e
Abraham Schilling <AbrahamSchilling@gmail.com>
2022-03-21 00:43:47 +0100

    Consume Spring Boot app as federation subgraph

--- src/server/apollo.ts
+++ src/server/apollo.ts
@@ -1,16 +1,37 @@
+import {
+  ApolloGateway,
+  GraphQLDataSourceProcessOptions,
+  IntrospectAndCompose,
+  RemoteGraphQLDataSource,
+} from "@apollo/gateway"
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core"
 import { ApolloServer } from "apollo-server-micro"
-import { authorResolvers } from "./author/resolvers"
-import { authorsTypeDefs } from "./author/typeDefs"
-import { bookResolvers } from "./book/resolvers"
-import { bookTypeDefs } from "./book/typeDefs"
-import { sharedTypeDefs } from "./shared/typeDefs"
+
+class AuthenticatedDataSource extends RemoteGraphQLDataSource {
+  willSendRequest({ request, context }: GraphQLDataSourceProcessOptions) {
+    const headers = (context as any)?.req?.headers || {}
+    for (const key in headers) {
+      const value = headers[key]
+      if (value) {
+        request.http?.headers.set(key, String(value))
+      }
+    }
+  }
+}
 
 export function createApolloServer() {
+  const gateway = new ApolloGateway({
+    supergraphSdl: new IntrospectAndCompose({
+      subgraphs: [{ name: "spring-boot", url: "http://localhost:8080/dgs" }],
+    }),
+    buildService({ name, url }) {
+      return new AuthenticatedDataSource({ url })
+    },
+  })
+
   return new ApolloServer({
-    typeDefs: [bookTypeDefs, authorsTypeDefs, sharedTypeDefs],
-    resolvers: [bookResolvers, authorResolvers],
     plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
     introspection: true,
+    gateway,
   })
 }
